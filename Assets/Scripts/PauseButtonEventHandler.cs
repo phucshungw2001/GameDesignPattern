45ad331bdcc33ae9bfe3a059f9ce46777be0340f
Handuong <handuong707@gmail.com>
2023-06-21 14:28:32 +0700

    done setting

--- Assets/Scripts/PauseButtonEventHandler.cs
+++ Assets/Scripts/PauseButtonEventHandler.cs
@@ -7,12 +7,16 @@ public class PauseButtonEventHandler : MonoBehaviour
 {
 
     private GameObject _pauseGamePanel;
+    private GameObject _settingPanel;
 
     // Start is called before the first frame update
     void Start()
     {
         _pauseGamePanel = GameObject.Find("PauseGamePanel");
+        _settingPanel = GameObject.Find("SettingPanel");
+
         _pauseGamePanel.SetActive(false);
+        _settingPanel.SetActive(false);
     }
 
     public void PauseGame()
@@ -32,6 +36,33 @@ public class PauseButtonEventHandler : MonoBehaviour
     public void SaveGame()
     {
         // TODO
+    }
+
+
+    public void Setting()
+    {
+        _pauseGamePanel.SetActive(false);
+        _settingPanel.SetActive(true);
+    }
+
+
+
+    public void TurnOnMusic()
+    {
+        // TODO
+    }
+
+
+    public void TurnOffMusic()
+    {
+        // TODO
+    }
+
+
+    public void BackToPausePanel()
+    {
+        _settingPanel.SetActive(false);
+        _pauseGamePanel.SetActive(true);
     }
 
     public void QuitGame()
