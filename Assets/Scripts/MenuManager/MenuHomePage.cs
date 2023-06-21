9f3a85c7101d6b11ebe49572d61e485cb0268be0
RoyTun <hungnphe150914@fpt.edu.vn>
2023-06-21 11:23:33 +0700

    update menu

--- Assets/Scripts/MenuManager/MenuHomePage.cs
+++ Assets/Scripts/MenuManager/MenuHomePage.cs
@@ -7,30 +7,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuHomePage : MonoBehaviour
 {
-
-/*    public string scoreKey = "Score";*/
-    public void onBtnExitClick()
+    public void onBtnPlayClick()
     {
-        Debug.Log("Quit!");
-        Application.Quit();
-    }
-
-    public void onBtnStartClick()
-    {
-/*        PlayerPrefs.SetInt("isLoad", 0);
-        PlayerPrefs.SetInt("save", 0);*/
         SceneManager.LoadScene("GameScenes");
     }
 
     public void onBtnLoadClick()
     {
-/*        PlayerPrefs.SetInt("isLoad", 1);*/
         SceneManager.LoadScene("GameScenes");
     }
 
-    public void onBtnSaveClick()
+    public void onBtnExitClick()
     {
-      
+        Debug.Log("Quit!");
+        Application.Quit();
     }
 
 }
